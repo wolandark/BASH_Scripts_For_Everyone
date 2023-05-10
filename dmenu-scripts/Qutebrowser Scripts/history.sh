881fac089e3fdda3667369d992dc8a2d8b84f585
wolandark <contact-woland@proton.me>
2023-05-10 23:43:27 +0330

    add info

--- dmenu-scripts/Qutebrowser Scripts/history.sh
+++ dmenu-scripts/Qutebrowser Scripts/history.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+# Written By Woland
+
+# Choose Files From Dmenu To Edit In Vim
+
+# Dependency:
+#          dmenu
+#          Qutebrowser
+
+# https://github.com/wolandark
+# https://github.com/wolandark/BASH_Scripts_For_Everyone
 
 db=$HOME/.local/share/qutebrowser/history.sqlite
 url=$(sqlite3 "$db" "select url, title, atime from History" | tac | awk -F '|' '{print $1}' | dmenu -l 20)
--- dmenu-scripts/Qutebrowser Scripts/quickmarks.sh
+++ dmenu-scripts/Qutebrowser Scripts/quickmarks.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#
+
+# Written By Woland
+
+# Choose Files From Dmenu To Edit In Vim
+
+# Dependency:
+#          dmenu
+#          Qutebrowser
+
+# https://github.com/wolandark
+# https://github.com/wolandark/BASH_Scripts_For_Everyone
+
 selected=$(cat /home/woland/.config/qutebrowser/quickmarks | sed 's/.*\(https:\/\/.*\)/\1/' | rofi -dmenu )
 
 if [ -z $selected ]; then
